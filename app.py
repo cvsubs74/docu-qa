33a2be048706e1ddb115e649883146ad978d1f7d
cvsubs74 <36245266+cvsubs74@users.noreply.github.com>
2023-05-02 22:01:06 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,85 @@
+import os
+import tempfile
+
+import streamlit as st
+from langchain.chains.question_answering import load_qa_chain
+from langchain.document_loaders import PyPDFLoader
+from langchain.indexes import VectorstoreIndexCreator
+from langchain.llms import AzureOpenAI
+
+from langchain.chains import RetrievalQA
+from langchain.indexes import VectorstoreIndexCreator
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+
+
+def main():
+    # Display introduction
+    display_introduction()
+    # Load llm
+    llm = load_llm()
+    # Get uploaded document
+    save_and_load_file(llm)
+    # Vectorize and query
+
+
+def display_introduction():
+    st.set_page_config(page_title="DocuQA", layout="wide")
+    st.header("DocuQA")
+    st.markdown("DocuQA is the ultimate tool for anyone who needs to work with "
+                "long PDF documents. Whether you're a busy professional trying to "
+                "extract insights from a report or a student looking for specific information "
+                "in a textbook, this powerful web app has the tools you need to get the "
+                "job done quickly and efficiently. With its intuitive interface and advanced "
+                "natural language processing capabilities, DocuQA is the perfect solution for "
+                "conducting question-answering tasks on long documents."
+                )
+
+
+def save_and_load_file(llm):
+    uploaded_file = st.file_uploader("Choose a document file", type=["pdf"])
+    loader = None
+    if uploaded_file is not None:
+        temp_dir = tempfile.TemporaryDirectory()
+        filepath = os.path.join(temp_dir.name, uploaded_file.name)
+        with open(filepath, "wb") as f:
+            f.write(uploaded_file.getbuffer())
+        if os.path.exists(filepath):
+            loader = PyPDFLoader(filepath)
+            if loader is not None:
+                document = loader.load()
+                """
+                # Query
+                chain = load_qa_chain(llm=llm, chain_type="map_reduce")
+                query = "Summarize the document"
+                st.write(chain.run(input_documents=document, question=query, verbose=True))
+                """
+                # split the documents into chunks
+                text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+                texts = text_splitter.split_documents(document)
+                # select which embeddings we want to use
+                embeddings = OpenAIEmbeddings()
+                # create the vector store to use as the index
+                db = FAISS.from_documents(texts, embeddings)
+                # expose this index in a retriever interface
+                retriever = db.as_retriever(search_type="similarity", search_kwargs={"k": 2})
+                # create a chain to answer questions
+                qa = RetrievalQA.from_chain_type(
+                    llm=llm, chain_type="stuff", retriever=retriever, return_source_documents=True)
+                query = "Summarize the document"
+                result = qa({"query": query})
+                st.write(result)
+
+
+def load_llm():
+    os.environ["OPENAI_API_TYPE"] = st.secrets["OPENAI_API_TYPE"]
+    os.environ["OPENAI_API_BASE"] = st.secrets["OPENAI_API_BASE"]
+    os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
+    llm = AzureOpenAI(temperature=0.9, deployment_name="text-davinci-003-dev1", model_name="text-davinci-003")
+    return llm
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == "__main__":
+    main()
