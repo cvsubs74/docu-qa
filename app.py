2f7b9fb9c34493da88904c109a4722963135f58a
cvsubs74 <36245266+cvsubs74@users.noreply.github.com>
2023-05-03 23:41:46 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
 import os
 import tempfile
+import tkinter
 
+import streamlit
 import streamlit as st
 from langchain.chains import RetrievalQA
 from langchain.document_loaders import PyPDFLoader
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.llms import AzureOpenAI
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.vectorstores import FAISS
+from langchain.vectorstores import Pinecone
+import pinecone
+import uuid
+
+index_name = "docuqa"
 
 
 def main():
@@ -15,12 +21,37 @@ def main():
     display_introduction()
     # Load llm
     llm = load_llm()
+    # Initialize the vector store
+    initialize_vector_store()
     # Get uploaded document
-    save_and_load_file(llm)
-    # Vectorize and query
+    uploaded_file = st.file_uploader("Choose a document file", type=["pdf"])
+    if uploaded_file:
+        vector_store = vectorize(uploaded_file)
+        # query
+        user_query(vector_store, llm)
+
+
+@st.cache_resource
+def initialize_vector_store():
+    # initialize pinecone
+    pinecone.init(
+        api_key=st.secrets["PINECONE_API_KEY"],
+        environment=st.secrets["PINECONE_ENV"]
+    )
+
+
+@st.cache_resource
+def load_llm():
+    print(">>load_llm")
+    os.environ["OPENAI_API_TYPE"] = st.secrets["OPENAI_API_TYPE"]
+    os.environ["OPENAI_API_BASE"] = st.secrets["OPENAI_API_BASE"]
+    os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
+    os.environ["PINECONE_API_KEY"] = st.secrets["PINECONE_API_KEY"]
+    return AzureOpenAI(temperature=0.9, deployment_name="text-davinci-003-dev1", model_name="text-davinci-003")
 
 
 def display_introduction():
+    print(">>display_introduction")
     st.set_page_config(page_title="DocuQA", layout="wide")
     st.header("DocuQA")
     st.markdown("DocuQA is the ultimate tool for anyone who needs to work with "
@@ -33,46 +64,38 @@ def display_introduction():
                 )
 
 
-def conversational_chat(chain, query):
-    result = chain({"question": query,
-                    "chat_history": st.session_state['history']})
-    st.session_state['history'].append((query, result["answer"]))
-    return result["answer"]
+@st.cache_resource
+def vectorize(uploaded_file):
+    print(">>vectorize")
+    index = pinecone.Index("docuqa")
+    vector_store = None
+    temp_dir = tempfile.TemporaryDirectory()
+    filepath = os.path.join(temp_dir.name, uploaded_file.name)
+    with open(filepath, "wb") as f:
+        f.write(uploaded_file.getbuffer())
+        loader = PyPDFLoader(filepath)
+        if loader is not None:
+            document = loader.load()
+            # split the documents into chunks
+            text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+            texts = text_splitter.split_documents(document)
+            # select which embeddings we want to use
+            embeddings = OpenAIEmbeddings(chunk_size=1)
+            # create the vector store to use as the index
+            vector_store = Pinecone.from_texts(
+                [t.page_content for t in texts], embeddings,
+                index_name=index_name, namespace=uploaded_file.name)
+    return vector_store
 
 
-def save_and_load_file(llm):
-    uploaded_file = st.file_uploader("Choose a document file", type=["pdf"])
-    loader = None
-    if uploaded_file is not None:
-        temp_dir = tempfile.TemporaryDirectory()
-        filepath = os.path.join(temp_dir.name, uploaded_file.name)
-        with open(filepath, "wb") as f:
-            f.write(uploaded_file.getbuffer())
-        if os.path.exists(filepath):
-            loader = PyPDFLoader(filepath)
-            if loader is not None:
-                document = loader.load()
-                # split the documents into chunks
-                text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-                texts = text_splitter.split_documents(document)
-                # select which embeddings we want to use
-                embeddings = OpenAIEmbeddings()
-                # create the vector store to use as the index
-                vector_store = FAISS.from_documents(texts, embeddings)
-                qa = RetrievalQA.from_chain_type(llm=llm, chain_type="map_reduce",
-                                                 retriever=vector_store.as_retriever())
-                query = st.text_area(label="Ask a question", placeholder="Your question..",
-                                     key="text_input")
-                if query:
-                    st.write(qa.run(query))
-
-
-def load_llm():
-    os.environ["OPENAI_API_TYPE"] = st.secrets["OPENAI_API_TYPE"]
-    os.environ["OPENAI_API_BASE"] = st.secrets["OPENAI_API_BASE"]
-    os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
-    llm = AzureOpenAI(temperature=0.9, deployment_name="text-davinci-003-dev1", model_name="text-davinci-003")
-    return llm
+def user_query(vector_store, llm):
+    print(">>user_query")
+    qa = RetrievalQA.from_chain_type(llm=llm, chain_type="map_reduce",
+                                     retriever=vector_store.as_retriever())
+    query = st.text_area(label="Ask a question", placeholder="Your question..",
+                         key="text_input", value="")
+    if query:
+        st.write(qa.run(query))
 
 
 # Press the green button in the gutter to run the script.
