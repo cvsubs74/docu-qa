0255c39cb6be0c9bf3c23c76646f7f90981e58c2
cvsubs74 <36245266+cvsubs74@users.noreply.github.com>
2023-05-04 22:55:57 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 import os
+import sqlite3
 import tempfile
+
+import pandas as pd
+import pinecone
+import plotly.express as px
 import streamlit as st
 from langchain.chains import RetrievalQA
 from langchain.document_loaders import PyPDFLoader
@@ -7,10 +12,6 @@ from langchain.embeddings import OpenAIEmbeddings
 from langchain.llms import AzureOpenAI
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import Pinecone
-import pinecone
-import sqlite3
-import plotly.express as px
-import pandas as pd
 
 index_name = "docuqa"
 dbname = "docuqa.db"
@@ -25,6 +26,8 @@ def main():
     initialize_vector_store()
     # Initialize document count store
     initialize_counter_store()
+    # Initialize review store
+    initialize_review_store()
     # Display introduction
     display_introduction()
     # Get uploaded document
@@ -33,8 +36,13 @@ def main():
         vector_store = vectorize_and_save(uploaded_file)
         # query
         user_query(vector_store, llm)
-    # Display Usage stats
-    display_stats()
+
+    col1, col2 = st.columns(2)
+    with col1:
+        display_stats()
+    with col2:
+        display_review_form()
+        display_review_stats()
 
 
 def display_stats():
@@ -68,12 +76,19 @@ def display_stats():
     )
 
     # Display the chart and heading in the Streamlit app
-    st.write("*App Usage Stats*")
+    st.subheader("App Usage Stats")
     st.plotly_chart(fig)
 
 
 def set_page_config():
     st.set_page_config(page_title="DocuQA", layout="wide")
+
+
+def slider_rating_widget():
+    st.subheader("Rating")
+    rating = st.slider("", 1, 5, key="slider_rating", format="%d",
+                       help="Drag the slider to rate the app")
+    return rating
 
 
 @st.cache_resource
@@ -86,14 +101,91 @@ def initialize_vector_store():
 
 
 def initialize_counter_store():
-    conn = sqlite3.connect(dbname)
-    c = conn.cursor()
-    c.execute('''CREATE TABLE IF NOT EXISTS doc_counters
+    connection = sqlite3.connect(dbname)
+    cursor = connection.cursor()
+    cursor.execute('''CREATE TABLE IF NOT EXISTS doc_counters
             (id INTEGER PRIMARY KEY, count INTEGER)''')
-    c.execute('''CREATE TABLE IF NOT EXISTS query_counters
+    cursor.execute('''CREATE TABLE IF NOT EXISTS query_counters
                 (id INTEGER PRIMARY KEY, count INTEGER)''')
-    conn.commit()
-    conn.close()
+    connection.commit()
+    connection.close()
+
+
+def add_review(rating, comment):
+    """Add a review to the database."""
+    connection = sqlite3.connect(dbname)
+    cursor = connection.cursor()
+    cursor.execute(f"INSERT INTO reviews (rating, comment) VALUES (?, ?)", (rating, comment))
+    connection.commit()
+
+
+# Define a function to display the review form
+# Define function to display review form and insert review into table on submission
+def display_review_form():
+    rating = slider_rating_widget()
+    comment = st.text_input("Please leave any comments or suggestions for improvement:")
+    if st.button("Submit Review", key="submit"):
+        if rating > 0:
+            add_review(rating, comment)
+            st.success("Thank you for your review!")
+        else:
+            st.warning("Please select a star rating before submitting.")
+
+
+# Define a function to display the review stats
+def display_review_stats():
+    num_reviews, avg_rating = get_review_stats()
+    st.subheader('**Average Rating**')
+    st.write(f'Average rating: {avg_rating} ({num_reviews} reviews)')
+    st.write('★' * int(round(avg_rating)) + '☆' * int(5 - round(avg_rating)))
+
+
+def get_review_stats():
+    connection = sqlite3.connect(dbname)
+    cursor = connection.cursor()
+    cursor.execute("SELECT COUNT(*), AVG(rating) FROM reviews")
+    result = cursor.fetchone()
+    num_reviews = result[0]
+    avg_rating = round(result[1], 1) if result[1] is not None else 0.0
+    return num_reviews, avg_rating
+
+
+def get_reviews():
+    """Get all the reviews from the database."""
+    connection = sqlite3.connect(dbname)
+    cursor = connection.cursor()
+    cursor.execute(f"SELECT rating, comment FROM reviews")
+    return cursor.fetchall()
+
+
+def display_reviews():
+    """Display all the reviews in a Streamlit app."""
+    reviews = get_reviews()
+
+    st.write("**Reviews**")
+    if not reviews:
+        st.write("No reviews yet. Be the first to leave a review!")
+        comment = st.text_input("Comment", "")
+        add_review()
+    else:
+        for rating, comment in reviews:
+            # Create a div element with the star rating
+            star_html = ""
+            for i in range(1, 6):
+                if i <= rating:
+                    star_html += '<i class="material-icons">star</i>'
+                else:
+                    star_html += '<i class="material-icons">star_border</i>'
+            st.write(f'<div class="review">{star_html}</div>', unsafe_allow_html=True)
+            st.write(f"Comment: {comment}")
+            st.write("---")
+
+
+def initialize_review_store():
+    connection = sqlite3.connect(dbname)
+    cursor = connection.cursor()
+    # Create the reviews table if it doesn't exist
+    cursor.execute(f"CREATE TABLE IF NOT EXISTS reviews (id INTEGER PRIMARY KEY, rating INTEGER, comment TEXT)")
 
 
 @st.cache_resource
@@ -119,20 +211,20 @@ def display_introduction():
 
 def retrieve_document_and_query_count():
     # Retrieve total document count from the database
+    connection = sqlite3.connect(dbname)
     document_count = 0
     query_count = 0
-    conn = sqlite3.connect(dbname)
-    c = conn.cursor()
-    c.execute('SELECT count FROM doc_counters WHERE id = 1')
-    count = c.fetchone()
+    cursor = connection.cursor()
+    cursor.execute('SELECT count FROM doc_counters WHERE id = 1')
+    count = cursor.fetchone()
     if count is not None:
         document_count = count[0]
 
-    c.execute('SELECT count FROM query_counters WHERE id = 1')
-    count = c.fetchone()
+    cursor.execute('SELECT count FROM query_counters WHERE id = 1')
+    count = cursor.fetchone()
     if count is not None:
         query_count = count[0]
-    conn.close()
+    connection.close()
     return document_count, query_count
 
 
@@ -153,30 +245,30 @@ def vectorize_and_save(uploaded_file):
 
 def add_document_counter():
     # Increment the document counter in the database
-    conn = sqlite3.connect(dbname)
-    c = conn.cursor()
-    c.execute('SELECT count FROM doc_counters WHERE id = 1')
-    count = c.fetchone()
+    connection = sqlite3.connect(dbname)
+    cursor = connection.cursor()
+    cursor.execute('SELECT count FROM doc_counters WHERE id = 1')
+    count = cursor.fetchone()
     if count is None:
-        c.execute('INSERT INTO doc_counters (id, count) VALUES (1, ?)', (1,))
+        cursor.execute('INSERT INTO doc_counters (id, count) VALUES (1, ?)', (1,))
     else:
-        c.execute('UPDATE doc_counters SET count = ? WHERE id = 1', (count[0] + 1,))
-    conn.commit()
-    conn.close()
+        cursor.execute('UPDATE doc_counters SET count = ? WHERE id = 1', (count[0] + 1,))
+    connection.commit()
+    connection.close()
 
 
 def add_query_counter():
     # Increment the query counter in the database
-    conn = sqlite3.connect(dbname)
-    c = conn.cursor()
-    c.execute('SELECT count FROM query_counters WHERE id = 1')
-    count = c.fetchone()
+    connection = sqlite3.connect(dbname)
+    cursor = connection.cursor()
+    cursor.execute('SELECT count FROM query_counters WHERE id = 1')
+    count = cursor.fetchone()
     if count is None:
-        c.execute('INSERT INTO query_counters (id, count) VALUES (1, ?)', (1,))
+        cursor.execute('INSERT INTO query_counters (id, count) VALUES (1, ?)', (1,))
     else:
-        c.execute('UPDATE query_counters SET count = ? WHERE id = 1', (count[0] + 1,))
-    conn.commit()
-    conn.close()
+        cursor.execute('UPDATE query_counters SET count = ? WHERE id = 1', (count[0] + 1,))
+    connection.commit()
+    connection.close()
 
 
 def vectorize(texts, uploaded_file):
